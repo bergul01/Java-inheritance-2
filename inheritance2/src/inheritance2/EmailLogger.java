07a424a3bf866f815fec6235a2f81aa03f7afe95
Berkay <gulberkay@hotmail.com>
2023-03-01 22:01:11 +0300

    Inheritance 2
    Inheritance çalışma kodlarım

---
+++ inheritance2/src/inheritance2/EmailLogger.java
@@ -0,0 +1,10 @@
+package inheritance2;
+
+public class EmailLogger extends Logger{
+	  //yazmasakta olur aşağıdaki kısım için geçerlidir.  3.gün 2:50:00 dk
+	public void log() {
+		System.out.println("Email yollandi");
+		
+	}
+
+}
